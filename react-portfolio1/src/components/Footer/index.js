bcc31537d2c7fb6b9ecac905888cd638597b4ec5
Trayehunter1 <hunterjohnsoninfo@gmail.com>
2021-09-18 13:55:08 -0500

    update info

--- react-portfolio1/src/components/Footer/index.js
+++ react-portfolio1/src/components/Footer/index.js
@@ -6,13 +6,13 @@ const Footer = () => {
     return (
         <div id='footer'>
             <p>
-                <a href='https://github.com/JJLindsey' target='_blank' rel='noopener noreferrer' alt='GitHub profile link'>
+                <a href='https://github.com/Trayehunter1' target='_blank' rel='noopener noreferrer' alt='GitHub profile link'>
                     <FaGithub/>
                 </a>
-                <a href='www.linkedin.com/in/jennifer-lindsey010' target='_blank' rel='noopener noreferrer' alt='Linked In profile link'>
+                <a href='https://www.linkedin.com/in/hunter-johnson-50b648182/' target='_blank' rel='noopener noreferrer' alt='Linked In profile link'>
                     <FaLinkedinIn/>
                 </a>
-                <a href='mailto:jlindsey010@gmail.com' target='_blank' rel='noopener noreferrer' alt='email link'>
+                <a href='hunterjohnsoninfo@gmail.com' target='_blank' rel='noopener noreferrer' alt='email link'>
                     <BsEnvelope/>
                 </a>
                 
